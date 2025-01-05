77b0fa540c9b332877b68b3e5518d1ddc935dd3d
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2025-01-05 11:47:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -186,11 +186,11 @@ def m(update, context):
                     if status_dolgnika == 'Долга нет':
                         text_image = 'DOLGA NET'
                         color_text = 'green'
-                    elif status_dolgnika == 'Должен за 2024':
+                    elif status_dolgnika == 'Должен за 2025':
                         text_image = f"DOLGEN ZA\n2024\nDOLG=\n={ostatok_dolga}"
                         color_text = 'yellow'
-                    elif status_dolgnika == 'Должен за 2023-2024':
-                        text_image = f"DOLGEN ZA\n2023\nI 2024\nDOLG=\n={ostatok_dolga}"
+                    elif status_dolgnika == 'Должен за 2024-2025':
+                        text_image = f"DOLGEN ZA\n2024\nI 2025\nDOLG=\n={ostatok_dolga}"
                         color_text = 'orange'
                     else:
                         text_image = f"ZLOCSTNIY\nDOLGNIK\nDOLG=\n={ostatok_dolga}"
